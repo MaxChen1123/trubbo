48df9349efe073ebe470b7f6075a03b45899ad69
MaxChen1123 <Zihowchen@gmail.com>
2024-11-05 21:06:36 +0800

    add KryoSerializer

---
+++ trubbo-remoting/src/main/java/com/maxchen/trubbo/remoting/codec/serialization/KyroSerializer.java
@@ -0,0 +1,13 @@
+package com.maxchen.trubbo.remoting.codec.serialization;
+
+public class KyroSerializer implements Serialization {
+    @Override
+    public byte[] serialize(Object object) throws Exception {
+        return KryoUtil.writeObjectToByteArray(object);
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) throws Exception {
+        return KryoUtil.readObjectFromByteArray(bytes, clazz);
+    }
+}
