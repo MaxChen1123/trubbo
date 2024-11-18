37a8ff853dba9682df70c76d4d95b28acbf9b4d7
MaxChen1123 <Zihowchen@gmail.com>
2024-11-18 23:48:02 +0800

    add Zookeeper test

---
+++ trubbo-remoting/src/test/java/ZookeeperTest/ZookeeperTest.java
@@ -0,0 +1,47 @@
+package ZookeeperTest;
+
+import com.maxchen.trubbo.common.URL.URL;
+import com.maxchen.trubbo.remoting.zookeeper.ZookeeperClient;
+import com.maxchen.trubbo.remoting.zookeeper.ZookeeperListener;
+import com.maxchen.trubbo.remoting.zookeeper.api.ListenerCallback;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
+import org.junit.jupiter.api.Test;
+
+public class ZookeeperTest {
+
+    @Test
+    public void client_test() throws Throwable {
+        ZookeeperClient zookeeperClient = new ZookeeperClient(new URL("zookeeper://127.0.0.1:2181"));
+        zookeeperClient.deletePath("/trubbo");
+        Thread.sleep(60 * 1000);
+    }
+
+    @Test
+    public void listener_test() throws Throwable {
+        ZookeeperClient zookeeperClient = new ZookeeperClient(new URL("zookeeper://127.0.0.1:2181"));
+        zookeeperClient.watchPath("/test/trubbo", new ZookeeperListener("/test/trubbo", new ListenerCallbackImpl()));
+        Thread.sleep(1000);
+        zookeeperClient.createPath("/test/trubbo/test", "test");
+        Thread.sleep(1000);
+        zookeeperClient.setData("/test/trubbo/test", "test changed");
+        Thread.sleep(1000);
+        zookeeperClient.deletePath("/test/trubbo/test");
+        Thread.sleep(1000);
+        zookeeperClient.deletePath("/test/trubbo");
+    }
+
+    static class ListenerCallbackImpl implements ListenerCallback {
+        @Override
+        public void callback(CuratorCacheListener.Type type, ChildData data, ChildData oldData) {
+            if (type == CuratorCacheListener.Type.NODE_DELETED) {
+                System.out.println("type: " + type + ", oldData's path: " + oldData.getPath() + " oldData's data" + new String(oldData.getData()));
+            }
+            if (data == null) {
+                System.out.println("type: " + type);
+                return;
+            }
+            System.out.println("type: " + type + ", data's path: " + data.getPath() + ", data's data: " + new String(data.getData()));
+        }
+    }
+}
